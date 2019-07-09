9d48e5e5c9468703a3d3b9447f1e421be0fe16b0
Joseph Zhang <jzhang268@DUEHOANG-7352.lan>
2019-07-08 18:14:10 -0700

    deleted:    pscircle/adwaita-night.png 	new file:   pscircle/aks-void-space.png 	modified:   pscircle/pscircle.sh

--- pscircle/pscircle.sh
+++ pscircle/pscircle.sh
@@ -4,7 +4,7 @@ output=/tmp/pscircle.png
 TIME_INTERVAL=10
 
 while [ 1 ]; do
-	pscircle --output=$output --output-width=1920 --output-height=1080 --background-image=/home/jzhang268/dotfiles/pscircle/adwaita-night.png --max-children=35 --collapse-threads=false --tree-sector-angle=3.9269 --tree-center=350:300 --tree-rotate=true --tree-rotation-angle=3.5342 --tree-radius-increment=110 --dot-radius=2 --link-width=1.3 --tree-font-size=11 --toplists-font-size=12 --toplists-bar-width=30 --toplists-row-height=15 --toplists-bar-height=3 --cpulist-center=-100:-300 --memlist-center=400:-300 --interval=0 
+	pscircle --output=$output --output-width=1920 --output-height=1080 --background-image=/home/jzhang268/dotfiles/pscircle/aks-void-space.png --background-image-scale=2 --max-children=35 --collapse-threads=false --tree-sector-angle=3.9269 --tree-center=350:300 --tree-rotate=true --tree-rotation-angle=3.5342 --tree-radius-increment=110 --dot-radius=2 --link-width=1.3 --tree-font-size=11 --toplists-font-size=12 --toplists-bar-width=30 --toplists-row-height=15 --toplists-bar-height=3 --cpulist-center=-100:-300 --memlist-center=400:-300 --interval=0 
 	sleep $TIME_INTERVAL
 	if command -v feh >/dev/null; then
         	feh --bg-scale  $output
